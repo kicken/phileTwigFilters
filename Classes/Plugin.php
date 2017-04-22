67597aff9b764beff3f3883996856ab11bac34e0
Keith M <keithm@aoeex.com>
2017-04-22 04:16:25 -0400

    Update for the new phile-core

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -2,48 +2,47 @@
 /**
  * Plugin class
  */
+
 namespace Phile\Plugin\Phile\TwigFilters;
+
+use Phile\Event\ServiceEvent;
+use Phile\Plugin\AbstractPlugin;
+use Phile\Plugin\TemplateTwig\TwigTemplatePlugin;
 
 /**
  * An example plugin showing how to make Twig filters
  * usage {{ content|excerpt }}
  */
-class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
-	/**
-	 * the constructor
-	 */
-	public function __construct() {
-		\Phile\Event::registerEvent('template_engine_registered', $this);
-	}
+class Plugin extends AbstractPlugin {
+    public static function getSubscribedEvents(){
+        return [
+            ServiceEvent::REGISTERED => 'onServiceRegistered'
+        ];
+    }
 
-	/**
-	 * event method
-	 *
-	 * @param string $eventKey
-	 * @param null   $data
-	 *
-	 * @return mixed|void
-	 */
-	public function on($eventKey, $data = null) {
-		if ($eventKey == 'template_engine_registered') {
-			// grab the first paragraph and remove all the html code
-			$excerpt = new \Twig_SimpleFilter('excerpt', function ($string){
-				return strip_tags(substr($string, 0, strpos($string, "</p>") + 4));
-			});
-			$data['engine']->addFilter($excerpt);
-			// limit words function -- very rough limit due to HTML input
-			$limit_words = new \Twig_SimpleFilter('limit_words', function ($string) {
-				$words = str_word_count($string, 2);
-				$nbwords = count($words);
-				$pos = array_keys($words);
-				if ($this->settings['limit'] >= $nbwords) {
-					return trim($string);
-				}
-				else {
-					return trim(substr($string, 0, $pos[$this->settings['limit']])) . '...';
-				}
-			});
-			$data['engine']->addFilter($limit_words);
-		}
-	}
+    public function onServiceRegistered(ServiceEvent $event){
+        $service = $event->getService();
+        if ($service instanceof TwigTemplatePlugin){
+            $twig = $service->getTwig();
+
+            // grab the first paragraph and remove all the html code
+            $excerpt = new \Twig_SimpleFilter('excerpt', function ($string){
+                return strip_tags(substr($string, 0, strpos($string, "</p>") + 4));
+            });
+            $twig->addFilter($excerpt);
+
+            // limit words function -- very rough limit due to HTML input
+            $limit_words = new \Twig_SimpleFilter('limit_words', function ($string){
+                $words = str_word_count($string, 2);
+                $nbwords = count($words);
+                $pos = array_keys($words);
+                if ($this->config['limit'] >= $nbwords){
+                    return trim($string);
+                } else {
+                    return trim(substr($string, 0, $pos[$this->config['limit']])) . '...';
+                }
+            });
+            $twig->addFilter($limit_words);
+        }
+    }
 }
